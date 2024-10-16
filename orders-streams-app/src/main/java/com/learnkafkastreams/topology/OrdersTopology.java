3fa7ab7f7bd8a0eedd1ceefd6b059c2131074b49
riparraguirre <riparraguirre@marketaxess.com>
2024-10-16 10:53:56 -0300

    aggregate orders total revenue by location id

--- orders-streams-app/src/main/java/com/learnkafkastreams/topology/OrdersTopology.java
+++ orders-streams-app/src/main/java/com/learnkafkastreams/topology/OrdersTopology.java
@@ -4,8 +4,11 @@ import com.learnkafkastreams.domain.*;
 import com.learnkafkastreams.util.OrderTimeStampExtractor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.support.serializer.JsonSerde;
 import org.springframework.stereotype.Component;
@@ -43,23 +46,7 @@ public class OrdersTopology {
 
         KTable<String,Store> storesTable = buildStoreTable(streamsBuilder);
 
-        orderStreams.split(Named.as("all-orders"))
-                    .branch(GENERAL_BRANCH_PREDICATE,
-                            Branched.withConsumer(generalOrdersStreams -> {
-                                generalOrdersStreams.print(Printed.<String,Order>toSysOut().withLabel("general-orders-stream"));
-                                generalOrdersStreams.mapValues(ORDER_TO_REVENUE_VALUE_MAPPER)
-                                                    .to(GENERAL_ORDERS, Produced.with(Serdes.String(), new JsonSerde<>(Revenue.class)));
-                                aggregateOrdersByCount(generalOrdersStreams, GENERAL_ORDERS_COUNT, storesTable);
-                            })
-                    )
-                    .branch(RESTAURANT_BRANCH_PREDICATE,
-                            Branched.withConsumer(restaurantOrdersStreams -> {
-                                restaurantOrdersStreams.print(Printed.<String,Order>toSysOut().withLabel("restaurant-orders-stream"));
-                                restaurantOrdersStreams.mapValues(ORDER_TO_REVENUE_VALUE_MAPPER)
-                                                       .to(RESTAURANT_ORDERS, Produced.with(Serdes.String(), new JsonSerde<>(Revenue.class)));
-                                aggregateOrdersByCount(restaurantOrdersStreams, RESTAURANT_ORDERS_COUNT, storesTable);
-                            })
-                    );
+        branchOrdersIntoTopics(orderStreams, storesTable);
     }
 
     private static KStream<String,Order> buildOrderStreams(StreamsBuilder streamsBuilder) {
@@ -84,16 +71,56 @@ public class OrdersTopology {
         return storesTable;
     }
 
+    private static void branchOrdersIntoTopics(KStream<String, Order> orderStreams, KTable<String, Store> storesTable) {
+        orderStreams.split(Named.as("all-orders"))
+                .branch(GENERAL_BRANCH_PREDICATE,
+                        Branched.withConsumer(generalOrdersStreams -> {
+                            generalOrdersStreams.print(Printed.<String,Order>toSysOut().withLabel("general-orders-stream"));
+                            generalOrdersStreams.mapValues(ORDER_TO_REVENUE_VALUE_MAPPER)
+                                                .to(GENERAL_ORDERS, Produced.with(Serdes.String(), new JsonSerde<>(Revenue.class)));
+
+                            aggregateOrdersByCount(generalOrdersStreams, GENERAL_ORDERS_COUNT, storesTable);
+                            aggregateTotalRevenueByLocationId(generalOrdersStreams, GENERAL_ORDERS_REVENUE);
+                        })
+                )
+                .branch(RESTAURANT_BRANCH_PREDICATE,
+                        Branched.withConsumer(restaurantOrdersStreams -> {
+                            restaurantOrdersStreams.print(Printed.<String,Order>toSysOut().withLabel("restaurant-orders-stream"));
+                            restaurantOrdersStreams.mapValues(ORDER_TO_REVENUE_VALUE_MAPPER)
+                                                   .to(RESTAURANT_ORDERS, Produced.with(Serdes.String(), new JsonSerde<>(Revenue.class)));
+
+                            aggregateOrdersByCount(restaurantOrdersStreams, RESTAURANT_ORDERS_COUNT, storesTable);
+                            aggregateTotalRevenueByLocationId(restaurantOrdersStreams, RESTAURANT_ORDERS_REVENUE);
+                        })
+                );
+    }
+
     private static void aggregateOrdersByCount(KStream<String, Order> ordersStreamsBranch, String ordersCountName, KTable<String, Store> storesTable) {
-        KTable<String,Long> ordersCountByStoreId = ordersStreamsBranch.groupByKey()
-                                                                     .count(Named.as(ordersCountName), Materialized.as(ordersCountName));
+        KTable<String,Long> ordersCountByStoreId = ordersStreamsBranch.map((key, value) -> KeyValue.pair(value.locationId(), value))
+                                                                      .groupByKey(Grouped.with(Serdes.String(), new JsonSerde<>(Order.class)))
+                                                                      .count(Named.as(ordersCountName), Materialized.as(ordersCountName));
         ordersCountByStoreId.toStream()
-                           .print(Printed.<String,Long>toSysOut().withLabel(ordersCountName));
+                            .print(Printed.<String,Long>toSysOut().withLabel(ordersCountName));
 
         ValueJoiner<Long, Store, TotalCountWithAddress> valueJoiner = TotalCountWithAddress::new;
 
         ordersCountByStoreId.join(storesTable, valueJoiner)
                             .toStream()
                             .print(Printed.<String, TotalCountWithAddress>toSysOut().withLabel(ordersCountName+"-by-store-id"));
+    }
+
+    private static void aggregateTotalRevenueByLocationId(KStream<String, Order> generalOrdersStreams, String storeName) {
+        Initializer<TotalRevenue> totalRevenueInitializer = TotalRevenue::new;
+
+        Aggregator<String, Order, TotalRevenue> totalRevenueAggregator = (key, order, aggregate) -> aggregate.updateRunningRevenue(key, order);
+
+        KeyValueMapper<String, Order, TotalRevenue> keyValueMapper = null;
+        generalOrdersStreams.map((key, value) -> KeyValue.pair(value.locationId(), value))
+                            .groupByKey(Grouped.with(Serdes.String(), new JsonSerde<>(Order.class)))
+                            .aggregate(totalRevenueInitializer, totalRevenueAggregator, Materialized.<String, TotalRevenue, KeyValueStore<Bytes, byte[]>>as(storeName)
+                                                                                                    .withKeySerde(Serdes.String())
+                                                                                                    .withValueSerde(new JsonSerde<>(TotalRevenue.class)))
+                            .toStream()
+                            .print(Printed.<String, TotalRevenue>toSysOut().withLabel(storeName));
     }
 }
